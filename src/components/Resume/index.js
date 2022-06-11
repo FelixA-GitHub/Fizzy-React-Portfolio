e139a151753d941b3b9df755205fda9f3caab654
Your Name <felix.acevedo.tech@gmail.com>
2022-06-11 15:09:46 -0400

    added resume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,13 +3,13 @@ import resume from '../../assets/resume/FullStackDeveloperResume.pdf';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Resume() {
-   const [pages] = useState([
-    {
-        name: "resume"
-    }
-   ]);
-   const [currentPage] = useState(pages[0]);
-   return (
+    const [pages] = useState([
+        {
+            name: "resume"
+        }
+    ]);
+    const [currentPage] = useState(pages[0]);
+    return (
         <section className=''>
             <div className='container'>
                 <div className='row'>
@@ -17,30 +17,33 @@ function Resume() {
                         <h1 className=''>{capitalizeFirstLetter(currentPage.name)}</h1>
                         <hr />
                         <h3>Front-End and Back-End Proficiencies</h3>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>Web APIs</li>
-                                <li>Third-Party APIs</li>
-                                <li>Server-Side APIs</li>
-                                <li>Node.js</li>
-                                <li>Object Oriented Programming</li>
-                                <li>Express.js</li>
-                                <li>SQL</li>
-                                <li>NoSQL</li>
-                                <li>Model-View-Controllers</li>
-                                <li>Object-Relational Mapping</li>
-                                <li>Progressive Web Applications</li>
-                                <li>React</li>
-                            </ul>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Web APIs</li>
+                            <li>Third-Party APIs</li>
+                            <li>Server-Side APIs</li>
+                            <li>Node.js</li>
+                            <li>Object Oriented Programming</li>
+                            <li>Express.js</li>
+                            <li>SQL</li>
+                            <li>NoSQL</li>
+                            <li>Model-View-Controllers</li>
+                            <li>Object-Relational Mapping</li>
+                            <li>Progressive Web Applications</li>
+                            <li>React</li>
+                        </ul>
+                        <p>
+                            Here is my <a href={resume} download>resume</a>
+                        </p>
                     </div>
 
                 </div>
 
             </div>
         </section>
-   )
+    );
 };
 
 
