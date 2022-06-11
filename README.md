3b80b03ec4d7e84cf04d73e1688bb1610253d6b2
Your Name <felix.acevedo.tech@gmail.com>
2022-06-11 13:54:59 -0400

    redoing nav file

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Fizzy-React-Portfolio
 
-
+<h2>
+        <a data-testid="link" href="/">
+        <span role="img" aria-label="briefcase">💼</span> Professional Portfolio
+        </a>
+      </h2>
 
 
             
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,54 +3,21 @@ import { capitalizeFirstLetter } from '../PhotoList/utils/helpers';
 
 function Nav(props) {
   const {
-    categories = [],
-    setCurrentCategory,
-    contactSelected,
-    currentCategory,
-    setContactSelected,
+    pages = [],
+    setCurrentPage,
+    currentPage
   } = props;
 
   useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
 
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-        <span role="img" aria-label="briefcase">💼</span> Professional Portfolio
-        </a>
-      </h2>
+
       <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
-          </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
+        
       </nav>
-    </header>
+
   );
 }
 
