74c87e59a8f07eedeee39faa46823530a0597879
Your Name <felix.acevedo.tech@gmail.com>
2022-06-11 14:23:42 -0400

    fleshing out renderPage

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,2 +1,20 @@
+//dependencies
 import React from "react";
-import 
+import PageContent from "../PageContent";
+import About from  "../About";
+import Portfolio from "../Portfolio";
+import Contact from "../Contact";
+import Resume from "../Resume";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Page({ currentPage }) {
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'about me':
+                return <About />;
+        }
+    }
+}
+
+
