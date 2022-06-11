87d88cfe32f27dab4853a9b83e9c38ca7dd00bda
Your Name <felix.acevedo.tech@gmail.com>
2022-06-11 14:28:44 -0400

    Page file completed

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,7 @@
 //dependencies
 import React from "react";
 import PageContent from "../PageContent";
-import About from  "../About";
+import About from "../About";
 import Portfolio from "../Portfolio";
 import Contact from "../Contact";
 import Resume from "../Resume";
@@ -13,8 +13,25 @@ function Page({ currentPage }) {
         switch (currentPage) {
             case 'about me':
                 return <About />;
+            case 'portfolio':
+                return <Portfolio />;
+            case 'contact':
+                return <Contact />;
+            case 'resume':
+                return <Resume />;
+            default:
+                return <About />;
         }
-    }
+    };
+
+    return (
+        <section>
+            <h2>{capitalizeFirstLetter(currentPage.name)}</h2>
+            <PageContent>{renderPage()}</PageContent>
+        </section>
+    );
 }
+
+export default Page;
 
 
