a9605ea5165def3596d4fa2dd444e88a26b32a82
Your Name <felix.acevedo.tech@gmail.com>
2022-06-18 14:31:02 -0400

    trying to get nav toggler working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -15,16 +15,16 @@ function Nav(props) {
   return (
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      
         <a className="navbar-brand" href="#home">Felix Acevedo</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             {pages.map((Page) => (
               <li
-                className={`nav-item nav-link active ${currentPage.name === Page.name && 'navActive'}`} key={Page.name}
+                className={`nav-item nav-link ${currentPage.name === Page.name && 'navActive'}`} key={Page.name}
               >
                 <span
                   onClick={() => setCurrentPage(Page)}
@@ -35,7 +35,7 @@ function Nav(props) {
             ))}
           </ul>
         </div>
-      </div>
+      
     </nav>
   );
 }
